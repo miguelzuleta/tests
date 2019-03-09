05646e8182cf9dded8fb1a69410165e8bf1127d5
Miguel Zuleta <mzuleta@apple.com>
2019-03-08 23:04:47 -0800

    add iframe

--- components/js/script1.js
+++ components/js/script1.js
@@ -1,3 +1,14 @@
-console.log('script1')
-
 //https://www.htmlgoodies.com/html5/css/display-iframe-contents-without-scrollbars.html
+
+let iframe = document.querySelector('iframe');
+
+iframe.onload = () => {
+	console.log('iframe loaded!')
+	scrollIframe();
+}
+
+let scrollIframe = () => {
+	window.addEventListener('scroll', () => {
+		iframe.contentWindow.scrollTo(0, window.pageYOffset);
+	})
+}
