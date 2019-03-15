c8c45e57d55acd65b2b9201bfd757babbc14ca80
Miguel Zuleta <mzuleta@apple.com>
2019-03-15 13:51:43 -0700

    add test files

--- components/js/script1.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// ReactDOM.render(
-//   <span>React Render</span>,
-//   document.getElementById('react-render')
-// )
-
-console.log('script1.js')
