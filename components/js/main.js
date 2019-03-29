dbc94eb38220943c2640c999698b528010b46323
Miguel Zuleta <mzuleta@apple.com>
2019-03-29 14:56:19 -0700

    add jumping clouds

--- components/js/main.js
+++ components/js/main.js
@@ -1,2 +1,3 @@
 // import test01 from './01-sock-merchant.js'
-import test02 from './02-counting-valleys.js'
+// import test02 from './02-counting-valleys.js'
+import test03 from './03-jumping-clouds.js'
