dcd2c51b4b0838b18bb94eb9fc774eb6d495ec72
Miguel Zuleta <mzuleta@apple.com>
2019-04-01 14:04:40 -0700

    add 05-array-ds

--- components/js/main.js
+++ components/js/main.js
@@ -1,4 +1,6 @@
 // import test01 from './01-sock-merchant.js'
 // import test02 from './02-counting-valleys.js'
 // import test03 from './03-jumping-clouds.js'
-import test04 from './04-repeated-string.js'
+// import test04 from './04-repeated-string.js'
+
+import test05 from './05-array-ds.js'
