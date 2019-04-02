19805ea35ea49f044c77006846e5d9598c8e4b14
Miguel Zuleta <mzuleta@apple.com>
2019-04-02 14:29:37 -0700

    finish array left rotation

--- components/js/06-array-left-rotation.js
+++ components/js/06-array-left-rotation.js
@@ -1,1 +1,13 @@
-// 
+let arr = [1, 2, 3, 4, 5];
+
+function left(a, d) {
+	for (let i = 0; i < d; i++) {
+		let first = a[0];
+		a.shift();
+		a.push(first);
+	}
+
+	return a.join(' ');
+}
+
+console.log(left(arr, 4));
