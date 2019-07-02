08514dc1042540a29f8968add676364d7cb658b1
mzuleta <zzuleta@hotmail.com>
2019-07-02 08:53:49 -0700

    simplify render functionality

--- components/js/script2.js
+++ components/js/script2.js
@@ -1,3 +1,88 @@
-let fn = (...args) => args.map(elem => elem / 2)
+let el = {
+    main: document.querySelector('main'),
+    list: null,
+    icon: {}
+};
 
-console.log( fn(4, 6, 9, 10, 1) )
+let load = file => {
+    return new Promise(resolve => {
+        fetch(file).then(response => response.json())
+                   .then(content => resolve(content));
+    });
+}
+
+let append = prop => {
+    let parentElement = prop.parent;
+    let childElement = document.createElement(prop.child);
+    let text = document.createTextNode(prop.text);
+
+    for (let key in prop.attrs) {
+        childElement.setAttribute(key, prop.attrs[key]);
+    }
+
+    if (prop.parent === undefined || prop.child === undefined) {
+        throw Error('parent or child properties missing from _append()');
+    }
+
+    if (prop.text) {
+        childElement.appendChild(text);
+    }
+
+    parentElement.appendChild(childElement);
+}
+
+let listSection = data => {
+    append({
+        parent: el.main,
+        child: 'h2',
+        text: 'Magical Title',
+        attrs: {
+            class: 'main-title'
+        }
+    });
+
+     append({
+        parent: el.main,
+        child: 'ul',
+        attrs: {
+            class: 'main-list'
+        }
+    });
+
+    el.list = el.main.querySelector('ul');
+
+    for (let elem in data.list) {
+        let { text, icon } = data.list[elem];
+
+        append({
+            parent: el.list,
+            child: 'li',
+            text: `${text} `,
+            attrs: {
+                class: `item item-${elem}`
+            }
+        });
+
+
+        el.icon[elem] = el.list.querySelector(`.item-${elem}`);
+
+        append({
+            parent: el.icon[elem],
+            child: 'span',
+            text: icon,
+            attrs: {
+                class: `icon icon-${elem}`
+            }
+        });
+    }
+}
+
+let loadData = load('./data.json');
+
+loadData.then(data => {
+    listSection(data);
+    // console.log(el.icon);
+    el.icon.three.addEventListener('click', event => {
+        console.log(event.target.innerHTML);
+    });
+});
