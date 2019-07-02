c5120cd94b0760be2eafb1dffebed1eacdf4cca5
mzuleta <zzuleta@hotmail.com>
2019-07-01 22:30:19 -0700

    add more tests

--- components/js/script2.js
+++ components/js/script2.js
@@ -18,4 +18,48 @@ let logElem = elem => {
     console.log(elem);
 }
 
-traverse(main, logElem);
+// traverse(main, logElem);
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+// girl ();
+
+let obj = {
+    mig: 'uel',
+    zul: 'eta',
+    likes: 'eating',
+    spicy: 'tacos',
+    deep: {
+        something: 'else',
+        lives: 'here'
+    }
+};
+
+let cloneObj = object => {
+    let newObject = {};
+
+    for (let key in object) {
+        newObject[key] = object[key];
+    }
+
+    return newObject;
+};
+
+// console.log(obj);
+
+// let newObj = cloneObj(obj);
+
+// newObj['YAYAYAYAYA'] = 'DSFU9S8DUF98SUDF';
+
+// console.log(obj);
+// console.log(newObj);
+
+let arr = 'miguel'.split('');
+console.log(arr);
+
+arr = ['BEGIN', ...arr, 'END'];
+console.log(arr);
+
