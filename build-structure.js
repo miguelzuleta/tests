1465007b1abae421a30ea2fa2e356a17ab0f7c26
mzuleta <zzuleta@hotmail.com>
2019-07-26 00:08:19 -0700

    Turn file structure function into a promise

--- build-structure.js
+++ build-structure.js
@@ -26,60 +26,75 @@ let fetch = url => {
 };
 
 let buildFileStructure = json => {
-    fetch(json).then(jsonFile => {
-        let data = JSON.parse(jsonFile);
-        let structure = data;
+    return new Promise(resolve => {
+        fetch(json).then(jsonFile => {
+            let data = JSON.parse(jsonFile);
+            let fileCount = {
+                fromFolders: 0,
+                writtenFiles: 0
+            };
 
-        let fileStructure = (rootPath, structureObj) => {
-            for (let folder in structureObj) {
-                let subFolder = structureObj[folder];
-                let isFolder = (folder.indexOf('.') < 0) && (typeof subFolder === 'object');
-                let fileType = isFolder ? 'folder' : 'file';
-                let newPath = `${rootPath}/${folder}`;
+            let fileStructure = (rootPath, structureObj) => {
+                for (let folder in structureObj) {
+                    let subFolder = structureObj[folder];
+                    let isFolder = (folder.indexOf('.') < 0) && (typeof subFolder === 'object');
+                    let fileType = isFolder ? 'folder' : 'file';
+                    let newPath = `${rootPath}/${folder}`;
 
-                if (fs.existsSync(newPath)) {
-                    console.log(`${fileType} "${folder}" already exists. Skipping...`);
+                    if (fs.existsSync(newPath)) {
+                        console.log(`${fileType} "${folder}" already exists. Skipping...`);
 
-                    if (isFolder) {
-                        fileStructure(newPath, subFolder);
-                    }
-                } else {
-                    if (isFolder) {
-                        console.log(`Creating ${fileType} "${folder}"`);
-                        
-                        fs.mkdir(newPath, err => {
-                            if (err) {
-                                throw err;
-                            }
-
+                        if (isFolder) {
                             fileStructure(newPath, subFolder);
-                        })
-                    } else {
-                        let writeFile = fileContent => {
-                            fs.writeFile(newPath, fileContent, err => {
-                                if (err) {
-                                    throw err;
-                                }
-
-                                console.log(`Creating "${folder}"`);
-                            });
-                        };
-
-                        if (subFolder.hasOwnProperty('remote')) {
-                            fetch(subFolder.content).then(fetchedContent => {
-                                writeFile(fetchedContent);
-                            });
-                        } else {
-                            writeFile(subFolder.content);
                         }
-                        
+                    } else {
+                        if (isFolder) {
+                            console.log(`Creating ${fileType} "${folder}"`);
+                            
+                            fs.mkdir(newPath, err => {
+                                if (err) throw err;
+
+                                let fileCountInFolder = Object.keys(subFolder).filter(file => {
+                                    return file.indexOf('.') > 0;
+                                }).length;
+
+                                fileCount.fromFolders += fileCountInFolder;
+
+                                fileStructure(newPath, subFolder);
+                            })
+                        } else {
+                            let writeFile = fileContent => {
+                                fs.writeFile(newPath, fileContent, err => {
+                                    if (err) throw err;
+
+                                    fileCount.writtenFiles++;
+
+                                    console.log(`Creating ${fileType} "${folder}"`);
+
+                                    if (fileCount.fromFolders === fileCount.writtenFiles) {
+                                        resolve();
+                                    }
+                                });
+                            };
+
+                            if (subFolder.hasOwnProperty('remote')) {
+                                fetch(subFolder.content).then(fetchedContent => {
+                                    writeFile(fetchedContent);
+                                });
+                            } else {
+                                writeFile(subFolder.content);
+                            }
+                            
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        fileStructure(__dirname, structure);
+            fileStructure(__dirname, data);
+        });
     });
 }
 
-buildFileStructure(`./test-01.json`);
+buildFileStructure(`./test-01.json`).then(() => {
+    console.log('DONE!')
+});
